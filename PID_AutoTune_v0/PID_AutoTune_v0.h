ad48e553f24ec3e0da759579b8b3329fece13542
cris8259 <huangjian8000@126.com>
2014-01-22 13:59:37 +0800

    Update PID_AutoTune_v0.h

--- PID_AutoTune_v0/PID_AutoTune_v0.h
+++ PID_AutoTune_v0/PID_AutoTune_v0.h
@@ -25,54 +25,22 @@ typedef struct
 	float Ku, Pu;	
 }PID_ATUNE_T;
 
-class PID_ATune
-{
+
+void PIDA_ATuneInit(PID_ATUNE_T *pid_atune, float* Input, float* Output)
+void PIDA_Cancel(PID_ATUNE_T *pid_atune);
+int PIDA_Runtime(PID_ATUNE_T *pid_atune);
+float PIDA_GetKp(PID_ATUNE *pid_atune);
+float PIDA_GetKi(PID_ATUNE *pid_atune);
+float PIDA_GetKd(PID_ATUNE *pid_atune);
+void PIDA_SetOutputStep(PID_ATUNE *pid_atune, float Step);
+float PIDA_GetOutputStep(PID_ATUNE *pid_atune);
+void PIDA_SetControlType(PID_ATUNE *pid_atune, int Type); //0=PI, 1=PID
+int PIDA_GetControlType(PID_ATUNE *pid_atune);
+void PIDA_SetNoiseBand(PID_ATUNE *pid_atune, float Band);
+float PIDA_GetNoiseBand(PID_ATUNE *pid_atune);
+void PIDA_SetLookbackSec(PID_ATUNE *pid_atune, int value);
+int PIDA_GetLookbackSec(PID_ATUNE *pid_atune);
 
 
-  public:
-  //commonly used functions **************************************************************************
-    PID_ATune(double*, double*);                       	// * Constructor.  links the Autotune to a given PID
-    int Runtime();						   			   	// * Similar to the PID Compue function, returns non 0 when done
-	void Cancel();									   	// * Stops the AutoTune	
-	
-	void SetOutputStep(double);						   	// * how far above and below the starting value will the output step?	
-	double GetOutputStep();							   	// 
-	
-	void SetControlType(int); 						   	// * Determies if the tuning parameters returned will be PI (D=0)
-	int GetControlType();							   	//   or PID.  (0=PI, 1=PID)			
-	
-	void SetLookbackSec(int);							// * how far back are we looking to identify peaks
-	int GetLookbackSec();								//
-	
-	void SetNoiseBand(double);							// * the autotune will ignore signal chatter smaller than this value
-	double GetNoiseBand();								//   this should be acurately set
-	
-	double GetKp();										// * once autotune is complete, these functions contain the
-	double GetKi();										//   computed tuning parameters.  
-	double GetKd();										//
-	
-  private:
-    void FinishUp();
-	bool isMax, isMin;
-	double *input, *output;
-	double setpoint;
-	double noiseBand;
-	int controlType;
-	bool running;
-	unsigned long peak1, peak2, lastTime;
-	int sampleTime;
-	int nLookBack;
-	int peakType;
-	double lastInputs[101];
-	double peaks[10];
-	int peakCount;
-	bool justchanged;
-	bool justevaled;
-	double absMax, absMin;
-	double oStep;
-	double outputStart;
-	double Ku, Pu;
-	
-};
 #endif
 
